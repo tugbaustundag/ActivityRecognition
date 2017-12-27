3a185ef6c9462bbed9f496ecefcac2f5d5f294cf
Tuğba Üstündağ <tugba.ust2008@gmail.com>
2017-12-27 16:05:50 +0200

    Activity Recognition ile Kullanıcı Aktivitelerini Tespit Etme
    Kullanıcı koşma, yürüme, bisiklet ya da araba sürme gibi aktiviteleri
    yaparken uygulama arka plan da dahi olsa, kullanıcının yaptığı
    aktiviteler hakkında bilgi vermeyi sağlayan Android uygulama

---
+++ ActivityRecognition/app/src/main/java/com/smality/activityrecognition/Constants.java
@@ -0,0 +1,11 @@
+package com.smality.activityrecognition;
+
+
+public class Constants {
+
+    public static final String BROADCAST_DETECTED_ACTIVITY = "activity_intent";
+
+    static final long DETECTION_INTERVAL_IN_MILLISECONDS = 30 * 1000;
+
+    public static final int CONFIDENCE = 70;
+}
---
+++ ActivityRecognition/app/src/main/java/com/smality/activityrecognition/DetectedActivitiesIntentService.java
@@ -0,0 +1,49 @@
+package com.smality.activityrecognition;
+
+
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import com.google.android.gms.location.ActivityRecognitionResult;
+import com.google.android.gms.location.DetectedActivity;
+
+import java.util.ArrayList;
+
+public class DetectedActivitiesIntentService  extends IntentService {
+
+    protected static final String TAG = DetectedActivitiesIntentService.class.getSimpleName();
+
+    public DetectedActivitiesIntentService() {
+        // Use the TAG to name the worker thread.
+        super(TAG);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        ActivityRecognitionResult result = ActivityRecognitionResult.extractResult(intent);
+
+        // Cihazın geçerli durumu ile ilgili olası aktivitelerin listesini alır
+        ArrayList<DetectedActivity> detectedActivities = (ArrayList) result.getProbableActivities();
+        //Döngü ile tüm yapılan faaliyetlerin tipini ve faaliyetin doğruluğu hakkında bilgi verir.(Değer 0 ile 100 arasındadır)
+        for (DetectedActivity activity : detectedActivities) {
+            Log.i(TAG, "Detected activity: " + activity.getType() + ", " + activity.getConfidence());
+            broadcastActivity(activity);
+        }
+    }
+
+    private void broadcastActivity(DetectedActivity activity) {
+        Intent intent = new Intent(Constants.BROADCAST_DETECTED_ACTIVITY);
+        intent.putExtra("type", activity.getType());
+        intent.putExtra("confidence", activity.getConfidence());
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+    }
+}
